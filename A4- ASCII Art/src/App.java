a0ccb14ee16e848be71e2d5efb4f66330d99128e
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-09-16 09:48:04 -0600

    A4- Made dragon

--- A4- ASCII Art/src/App.java
+++ A4- ASCII Art/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(" <>=======()");
+        System.out.println("(/\\___   /|\\\\          ()==========<>_");
+        System.out.println("      \\_/ | \\\\        //|\\   ______/ \\)");
+        System.out.println("        \\_|  \\\\      // | \\_/");
+        System.out.println("         \\|\\/|\\_   //  /\\/");
+        System.out.println("           (oo)\\ \\_//  /");
+        System.out.println("          //_/\\_\\/ /  |");
+        System.out.println("         @@/  |=\\  \\  |");
+        System.out.println("              \\_=\\_ \\ |");
+        System.out.println("                \\==\\ \\|\\_");
+        System.out.println("            ___(\\===\\(  )\\");
+        System.out.println("           (((~) __(_/   |");
+        System.out.println("                (((~) \\  /");
+        System.out.println("                 _____/ /");
+        System.out.println("                '------'");
+
     }
 }
