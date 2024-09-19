6f25cea1972df6c91dace4e8810accdd6f068084
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-09-19 09:45:34 -0600

    String Lesson

---
+++ 1-Lessons And Demos/Lesson7 Strings/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // String greeting = new String("This is my message");
+        // String name = "Fox";
+        String name = new String("Fox");
+        System.out.println(name.equals("Fox"));
+
+        String message1= "This is a test";
+        System.out.println(message1.length());
+        System.out.println(message1.substring(0,3));
+        System.out.println(message1.indexOf("is"));
+        System.out.println(message1.indexOf("Hello"));
+
+
+
+
+
+
+
+
+    }
+}
