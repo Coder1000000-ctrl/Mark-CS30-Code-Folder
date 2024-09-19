9ddc44bca0568520bf03a2a1b21c37c0217be979
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-09-19 09:22:04 -0600

    Wing Calculator

--- A6- Wing Calculator/src/App.java
+++ A6- Wing Calculator/src/App.java
@@ -7,12 +7,12 @@ Program Description: A calculator used the find how many wings you can buy.*/
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int wings;
-        double cost;
-        double money;
+        int wings; //how many wings you can buy
+        double cost; //the cost of each wing
+        double money; //the amount of money you have
         money = 4.75;
         cost = 0.75;
-        wings = (int)(money /cost);
+        wings = (int)(money /cost); //calculating how many wings you can afford
         System.out.println("You can afford to buy " + wings + " wings, if you have " + money + " dollars and each wing costs " + cost + " dollars.");
     }
 }
