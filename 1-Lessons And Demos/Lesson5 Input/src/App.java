25dfe7c149b5323b1e69b9952aa8c86a3de943d9
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-09-23 09:47:13 -0600

    Lesson 5

--- 1-Lessons And Demos/Lesson5 Input/src/App.java
+++ 1-Lessons And Demos/Lesson5 Input/src/App.java
@@ -5,9 +5,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner keys = new Scanner(System.in);
 
-        // System.out.print("Enter a number: ");
-        // int num = keys.nextInt();
-        // System.out.println("You chose " + num);
+        System.out.print("Enter a number: ");
+        int num = keys.nextInt();
+        keys.nextLine();
+        System.out.println("You chose " + num);
 
         System.out.print("What is your name? ");
         String name = keys.nextLine();
