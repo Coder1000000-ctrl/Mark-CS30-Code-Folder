05e3d2ed3d73ce1345afdd9d4825cac7e5921642
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-09-23 09:46:13 -0600

    Lesson 5

---
+++ 1-Lessons And Demos/Lesson5 Input/src/App.java
@@ -0,0 +1,16 @@
+//Imports the Scanner Class into the program
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner keys = new Scanner(System.in);
+
+        // System.out.print("Enter a number: ");
+        // int num = keys.nextInt();
+        // System.out.println("You chose " + num);
+
+        System.out.print("What is your name? ");
+        String name = keys.nextLine();
+        System.out.println("You are " + name);
+    }
+}
