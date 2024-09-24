b63fddbf933aaacce7aee451ddb7858bbfccf88b
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-09-24 08:52:50 -0600

    comments

--- A7- String Basics/src/App.java
+++ A7- String Basics/src/App.java
@@ -11,12 +11,12 @@ public class App {
         String target1 = "very ";
         int length1 = target1.length();
         int startOne = message1.indexOf(target1);
-        System.out.println(message1.substring(0, startOne) + message1.substring((startOne + length1)));
+        System.out.println(message1.substring(0, startOne) + message1.substring((startOne + length1))); //Prints the first half then the part after very.
 
         String message2 = "That was great - lol.";
         String target2 = "lol";
         String replacement = "laugh out loud.";
         int startTwo = message2.indexOf(target2);
-        System.out.println(message2.substring(0, startTwo) + replacement);
+        System.out.println(message2.substring(0, startTwo) + replacement); //Prints the first half then replaces lol with "laugh out loud"
     }
 }
