7af281457a1417ea689b708ede9adb19529b4c78
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-01 09:22:19 -0600

    done methods

---
+++ A8b - Methods/src/Baker.java
@@ -0,0 +1,16 @@
+public class Baker {
+
+    //Code for the types of bread
+    public void typesOfBread() {
+        System.out.println("Breads:\nWhite bread\nWholewheat bread\nRye bread\nSourdough bread\nMultigrain bread\nBaguette\nCiabatta\nPumpernickle bread\nChallah\nBrioche\nFlatbread\nBagel\nFocaccia\nCornbread\nSoda bread");
+
+    }
+
+    //code for the types of desserts
+    public void typesOfDesserts(){
+        System.out.println("\nDesserts:\nCakes\nCookies\nBiscuits\nGelatins\nPastries\nIce creams\nPies\nPuddings\nCandies");
+  
+    }
+
+
+}
---
+++ A8b - Methods/src/BakerRunner.java
@@ -0,0 +1,14 @@
+// Program name: Baker Mike
+// Student name: Mark Tucker
+// Date: October 1, 2024
+// Program Description: Create a sub class with two methods. Create a runner class that calls those methods and shows an output.
+
+
+public class BakerRunner {
+    public static void main(String[] args) throws Exception {
+        Baker mike = new Baker(); //Creates a new Baker called mike
+        mike.typesOfBread(); //Outputs many different types of bread
+        mike.typesOfDesserts(); //Outputs many different types of desserts
+
+    }
+}
