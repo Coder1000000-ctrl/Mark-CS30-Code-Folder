7bbe36bbb616e571b66e30bfd6ab4894b7345f75
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-04 09:46:08 -0600

    inout

--- A10- Averages/src/Sum.java
+++ A10- Averages/src/Sum.java
@@ -4,6 +4,19 @@ public class Sum {
     private int secondNum = 0;
     Scanner keys = new Scanner(System.in);
     public void setNums(int a, int b) {
+        System.out.println("Enter a number");
+        firstNum = keys.nextInt();
+        keys.nextLine();
+
+        System.out.println("Enter a number");
+        secondNum = keys.nextInt();
+        keys.nextLine();
+
+        public void sum(int a, int b) {
+            
+        }
+
+
 
 
 
