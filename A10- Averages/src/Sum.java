86762227e5a59514d691ab924fa4d21b23b416e3
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-04 09:45:41 -0600

    created input

---
+++ A10- Averages/src/Sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sum {
+    private int firstNum = 0;
+    private int secondNum = 0;
+    Scanner keys = new Scanner(System.in);
+    public void setNums(int a, int b) {
+
+
+
+}
+}
