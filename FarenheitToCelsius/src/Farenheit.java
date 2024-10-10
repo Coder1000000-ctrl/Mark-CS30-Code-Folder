cc4eeb4126a32243b6a5d93204448014944854c4
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-10 09:38:34 -0600

    farenheit to celsius

--- FarenheitToCelsius/src/Farenheit.java
+++ FarenheitToCelsius/src/Farenheit.java
@@ -1,12 +1,15 @@
 public class Farenheit {
 
+    // creates celsius
     private double celsius;
 
 
+    // calculates farenheit to celsius
     public void calculate(double input){
         celsius = (input-32) * ((double)5/9);
     }
 
+    // outputs celsius
     public void output(double input){
         System.out.println(input + "°f is " + celsius + "°c.");
     }
--- FarenheitToCelsius/src/FarenheitRunner.java
+++ FarenheitToCelsius/src/FarenheitRunner.java
@@ -1,14 +1,21 @@
-import java.util.Scanner;
+// Program name: Farenheit to Celsius
+// Student name: Mark Tucker
+// Date: October 10, 2024
+// Program Description: Converts Farenheit to Celsius
+
+import java.util.Scanner; // imports a scanner
 
 public class FarenheitRunner {
     public static void main(String[] args) throws Exception {
-        Scanner keys = new Scanner(System.in);
-        double farenheit;
-        Farenheit converter = new Farenheit();
+        Scanner keys = new Scanner(System.in); // creates a scanner
+        double input; // creates variable called input
+        Farenheit converter = new Farenheit(); // imports methods
 
         System.out.print("What temperature in farenheit do you want to convert to celsius: ");
-        farenheit = keys.nextDouble();
-        converter. calculate(farenheit);
-        converter. output(farenheit);
+        input = keys.nextDouble(); // takes in input
+
+        // calculates and outputs the conversion
+        converter. calculate(input);
+        converter. output(input);
     }
 }
