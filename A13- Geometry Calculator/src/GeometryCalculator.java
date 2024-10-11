c3e74300423b2fc20b439ae50e5437f31b6ea754
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-11 09:49:12 -0600

    geometry

--- A13- Geometry Calculator/src/GeometryCalculator.java
+++ A13- Geometry Calculator/src/GeometryCalculator.java
@@ -1,3 +1,27 @@
+
 public class GeometryCalculator {
+    private double surfaceArea = 0;
+
+
+    public double circle(double rad){
+        surfaceArea = Math.PI * (rad * rad);
+        return surfaceArea;
+    }
+    public double cube(double side){
+        surfaceArea = 6* (side * side);
+        
+        return surfaceArea;
+        
+    }
+    public double rectangle(double len, double wid){
+        surfaceArea = len* wid;
+        
+        return surfaceArea;
+        
+    }
+    public void output(double out){
+        System.out.println("The surface area is " + out + "units.");
+    }
+
 
 }
--- A13- Geometry Calculator/src/GeometryCalculatorRunner.java
+++ A13- Geometry Calculator/src/GeometryCalculatorRunner.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
 public class GeometryCalculatorRunner {
     public static void main(String[] args) throws Exception {
+        int choice = 0;
+        double area = 0;
+        double radius = 0;
+        double sideLength = 0;
+        double length = 0;
+        double width = 0;
+        
+        GeometryCalculator calc = new GeometryCalculator();
+        Scanner keys = new Scanner(System.in);
+        System.out.print("Would you like to know the area of a \n1) Circle:\n2) Cube\n3) Rectangle\nEnter Choice: ");
+        choice = keys.nextInt();
+        if (choice == 1){
+            System.out.println("What is the radius: ");
+            radius = keys.nextDouble();
+            area =calc.circle(radius);
+
+        }
+        if (choice == 2){
+            System.out.print("What is the side length: ");
+            sideLength = keys.nextDouble();
+            area =calc.cube(sideLength);
+
+        }
+        if (choice == 3){
+            System.out.print("What is the length: ");
+            length = keys.nextDouble();
+            keys.nextLine();
+            System.out.print("What is the width: ");
+            width = keys.nextDouble();
+            area =calc.rectangle(length, width);
+            
+        }
+        
+        keys.close();
+        calc.output(area);
         
     }
 }
