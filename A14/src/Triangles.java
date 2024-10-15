ec9419dd2b8518be3aa976e9efa3c4d360e9b1fb
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-15 09:46:30 -0600

    triangles

---
+++ A14/src/Triangles.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Triangles {
+    Scanner keys = new Scanner(System.in);
+    public Triangles(){
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double s = 0;
+    }
+    public double input(){
+        double num = keys.nextDouble();
+        return num;
+
+    }
+    //calc
+
+}
---
+++ A14/src/TrianglesRunner.java
@@ -0,0 +1,8 @@
+public class TrianglesRunner {
+    public static void main(String[] args) throws Exception {
+        //3 things asking input
+        
+        //calc
+        // out
+    }
+}
