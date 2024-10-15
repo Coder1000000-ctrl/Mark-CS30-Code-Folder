80aee0dab3ee064a5d03a89ef8c3cbeb7f0838e3
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-15 09:01:53 -0600

    area

--- A13- Geometry Calculator/src/GeometryCalculator.java
+++ A13- Geometry Calculator/src/GeometryCalculator.java
@@ -1,26 +1,30 @@
-
 public class GeometryCalculator {
+
+    // defines variables
     private double surfaceArea = 0;
 
-
+    // runs calcualtions for circle
     public double circle(double rad){
         surfaceArea = Math.PI * (rad * rad);
         return surfaceArea;
     }
+    // runes calculations for cube
     public double cube(double side){
         surfaceArea = 6* (side * side);
         
         return surfaceArea;
         
     }
+    // runes calculations for cube
     public double rectangle(double len, double wid){
         surfaceArea = len* wid;
         
         return surfaceArea;
         
     }
+    //outputs the area
     public void output(double out){
-        System.out.println("The surface area is " + out + "units.");
+        System.out.println("The surface area is " + out + " units.");
     }
 
 
--- A13- Geometry Calculator/src/GeometryCalculatorRunner.java
+++ A13- Geometry Calculator/src/GeometryCalculatorRunner.java
@@ -1,6 +1,13 @@
-import java.util.Scanner;
+// Program name: Geometry Calculator
+// Student name: Mark Tucker
+// Date: October 15, 2024
+// Program Description: a program that calculates the surface area of a shape
+
+import java.util.Scanner; // imports scanner
+
 public class GeometryCalculatorRunner {
     public static void main(String[] args) throws Exception {
+        // defines variables
         int choice = 0;
         double area = 0;
         double radius = 0;
@@ -8,34 +15,37 @@ public class GeometryCalculatorRunner {
         double length = 0;
         double width = 0;
         
-        GeometryCalculator calc = new GeometryCalculator();
-        Scanner keys = new Scanner(System.in);
-        System.out.print("Would you like to know the area of a \n1) Circle:\n2) Cube\n3) Rectangle\nEnter Choice: ");
+        GeometryCalculator calc = new GeometryCalculator(); // creates new object
+
+        Scanner keys = new Scanner(System.in); // creates a scanner
+
+
+        // takes in choice and does calculations
+        System.out.print("Would you like to know the area of a \n1) Circle\n2) Cube\n3) Rectangle\nEnter Choice: ");
         choice = keys.nextInt();
         if (choice == 1){
-            System.out.println("What is the radius: ");
+            System.out.print("What is the radius: ");
             radius = keys.nextDouble();
             area =calc.circle(radius);
-
         }
+
         if (choice == 2){
             System.out.print("What is the side length: ");
             sideLength = keys.nextDouble();
             area =calc.cube(sideLength);
-
         }
+
         if (choice == 3){
             System.out.print("What is the length: ");
             length = keys.nextDouble();
             keys.nextLine();
             System.out.print("What is the width: ");
             width = keys.nextDouble();
-            area =calc.rectangle(length, width);
-            
+            area =calc.rectangle(length, width); 
         }
         
         keys.close();
-        calc.output(area);
+        calc.output(area); //outputs the area
         
     }
 }
