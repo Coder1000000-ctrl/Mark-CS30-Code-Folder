22a93926ec987f6076d574e43881e96ce15a0a5f
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-16 09:39:19 -0600

    triangle

--- A14/src/Triangles.java
+++ A14/src/Triangles.java
@@ -1,17 +1,31 @@
-import java.util.Scanner;
+import java.util.Scanner; // imports a scanner
+
 public class Triangles {
-    Scanner keys = new Scanner(System.in);
+    Scanner keys = new Scanner(System.in); // creates a scanner
+    // defines variabels
+    private double s;
+    private int sideNum;
+
+    // constructor
     public Triangles(){
-        double a = 0;
-        double b = 0;
-        double c = 0;
-        double s = 0;
+        s = 0;
+        sideNum = 0;
     }
+    // takes in input
     public double input(){
+        sideNum++;
+        System.out.print("What is the length of side " + sideNum + ": ");
         double num = keys.nextDouble();
+        keys.nextLine();
         return num;
 
     }
-    //calc
+    // calculates area
+    public double calculate(double a, double b, double c){
+        double perimeter = a + b + c;
+        s = perimeter / 2;
+        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        return area;
+    }
 
 }
--- A14/src/TrianglesRunner.java
+++ A14/src/TrianglesRunner.java
@@ -1,8 +1,20 @@
+// Program name: Triangles
+// Student name: Mark Tucker
+// Date: October 16, 2024
+// Program Description: Calculates the area of a triangle.
+
 public class TrianglesRunner {
     public static void main(String[] args) throws Exception {
-        //3 things asking input
-        
-        //calc
-        // out
+        Triangles triangle = new Triangles(); // creates a new object
+
+        // defines variables
+        double sideOne = triangle.input();
+        double sideTwo = triangle.input();
+        double sideThree= triangle.input();
+        double surfaceArea = 0;
+        // calculates area
+        surfaceArea = triangle.calculate(sideOne,sideTwo,sideThree);
+        // outputs area
+        System.out.println("The area of the trianble is " + surfaceArea +"units.");
     }
 }
