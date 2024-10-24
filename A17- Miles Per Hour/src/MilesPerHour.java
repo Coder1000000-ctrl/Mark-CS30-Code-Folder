f637262155e36452dbf017d3acd7c614651066df
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-24 09:16:55 -0600

    done

--- A17- Miles Per Hour/src/MilesPerHour.java
+++ A17- Miles Per Hour/src/MilesPerHour.java
@@ -1,11 +1,24 @@
-import java.util.Scanner;
+import java.util.Scanner; // imports scanner
+
 public class MilesPerHour {
+    // creates a scanner
     Scanner keys = new Scanner(System.in);
 
-    public int ask(String asking) {
+    // takes in input
+    public double ask(String asking) {
         System.out.print("Enter the " + asking + ": ");
-        int answer = keys. nextInt();
+        double answer = keys.nextDouble();
         return answer;
+    }
+    // calculates time
+    public double minutes(double time) {
+        time = time / 60;
+        return time;
+    }
+    // calculates rate
+    public double milesAnHour(double far, double totalTime){
+        double rate = far / totalTime;
+        return rate;
     }
    
 
--- A17- Miles Per Hour/src/MilesPerhourRunner.java
+++ A17- Miles Per Hour/src/MilesPerhourRunner.java
@@ -1,8 +1,21 @@
+// Program name: Miles Per Hour
+// Student name: MArk Tucker
+// Date: OCtober 24, 2024
+// Program Description: Calculates the miles per hour from a time and distance.
+
 public class MilesPerhourRunner {
     public static void main(String[] args) throws Exception {
+        // creates a new object called miles
         MilesPerHour miles = new MilesPerHour();
-        int distance = miles.ask("Distance");
-        int hours = miles.ask("Hours");
-        int minutes = miles.ask("Minutes");
+        // creates variables and gets input
+        double distance = miles.ask("Distance");
+        double hours = miles.ask("Hours");
+        double minute = miles.ask("Minutes");
+
+        double newTime = hours + miles.minutes(minute); // calculates the total time in hours
+        double speed = miles.milesAnHour(distance, newTime); // calculates the speed
+
+        // outputs the rate, distance, and time rounded to a whole number.
+        System.out.println(Math.round(distance) + " miles in "+ Math.round(hours) + " hours and " + Math.round(minute) + " minutes = " + Math.round(speed) + " MPH.");
     }
 }
