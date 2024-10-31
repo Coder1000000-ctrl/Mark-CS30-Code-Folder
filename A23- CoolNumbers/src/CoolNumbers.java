1b3b92eae3f34f09efd8867ed5c3c4289b515085
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-10-31 09:00:22 -0600

    done

--- A23- CoolNumbers/src/CoolNumbers.java
+++ A23- CoolNumbers/src/CoolNumbers.java
@@ -1,6 +1,16 @@
+import java.util.Scanner; // imports scanner
+
 public class CoolNumbers {
-    int count;
-    public boolean isCoolNum( int checkN ) {
+    int count; // creates count
+
+    public int input(){ // takes in input
+        Scanner keys = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = keys.nextInt();
+        keys.close();
+        return number;
+    }
+    public boolean isCoolNum( int checkN ) { // checks if it is a cool number
         if (checkN%6 == 1 && checkN%5 == 1 && checkN%4 == 1 && checkN%3 == 1){
             return true;
         }
@@ -8,7 +18,7 @@ public class CoolNumbers {
             return false;
         }
     }
-    public int countCoolNumbers( int n ){
+    public int countCoolNumbers( int n ){ // checks how many cool numbers there are in a range
         for (int i=n; i >= 6; i--){
             if(isCoolNum(i)){
                 count++;
--- A23- CoolNumbers/src/CoolNumbersRunner.java
+++ A23- CoolNumbers/src/CoolNumbersRunner.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-
+// Program name: CoolNumbers
+// Student name: Mark Tucker
+// Date: October 31, 2024
+// Program Description: Checks how many numbers are divisible by 3,4,5, and 6 in a range.
 
 public class CoolNumbersRunner {
     public static void main(String[] args) throws Exception {
-        CoolNumbers cool = new CoolNumbers();
-        Scanner keys = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = keys.nextInt();
-        int total = cool.countCoolNumbers(number);
-        keys.close();
-        System.out.println("There are " + total + " cool numbers between 6-" + number + ".");
+        CoolNumbers cool = new CoolNumbers(); // creates a new object called cool
+
+        int userNumber = cool.input(); // takes in input
+        int total = cool.countCoolNumbers(userNumber); // calculates the total numbers divisble by 3,4,5, and 6
+        System.out.println("There are " + total + " cool numbers between 6-" + userNumber + "."); // outputs the total
 
 
 
