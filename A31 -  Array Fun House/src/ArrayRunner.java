56161f3eee0792fc5d553bd20e12b7fe429960fd
Coder1000000-ctrl <mark.tucker@rcsd.ca>
2024-11-21 09:30:06 -0600

    some thursday things

---
+++ A31 -  Array Fun House/src/ArrayRunner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class ArrayRunner {
+    public static void main(String[] args) throws Exception {
+        Scanner keys = new Scanner(System.in);
+        keys.close();
+
+        
+    }
+}
---
+++ A31 -  Array Fun House/src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public void Array(int[] array) {
+        int[] list = array;
+    }
+
+    public int sum(int start, int stop){
+        for (int i = start;)
+    }
+    
+}
+
